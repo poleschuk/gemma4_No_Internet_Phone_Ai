5850e6772de1787c6b1c88e27edb407c573a32e3
Иван Полещук <wellington101823@gmail.com>
2026-04-19 01:49:20 +0300

    first upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import torch
+from transformers import AutoProcessor, AutoModelForCausalLM
+
+from config.config import system_prompt
+from config.paths import MODEL_PATH
+
+from memory.agent_memory import AgentMemory
+
+from AI.ai import AI
+
+if __name__ == "__main__":
+    memory = AgentMemory(max_items=10)
+    agent = AI(MODEL_PATH)
+    while(True):
+        print(f"User: ")
+        user_input = input()
+        
+        memory.add_message(f"User: {user_input}")
+        
+        context = memory.get_context()
+        
+        messages = [{"role": "system", "content": context}]
+        messages.append({"role": "user", "content": user_input})
+        
+        response = agent.generate(messages)
+        memory.add_message(f"User: {user_input}")
+        memory.add_message(f"Agent: {response}")
+        
+        print(f"AI: ")
+        print(response)
